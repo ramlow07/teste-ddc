fd589ff5be930bcc8ddf114a985018314e201df0
ramlow07 <luamramlow@gmail.com>
2025-07-13 11:00:24 -0300

    updating auth entities

--- backend/src/modules/auth/entities/entities.ts
+++ backend/src/modules/auth/entities/entities.ts
@@ -1,6 +1,6 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { z } from 'zod';
-import { MainResponse } from '../@shared/graphql/types';
+import { MainResponse } from '../../../@shared/graphql/types';
 
 @ObjectType('AuthResponse')
 export class AuthResponse extends MainResponse {
@@ -8,6 +8,24 @@ export class AuthResponse extends MainResponse {
   token?: string;
 }
 
+
+@InputType('SignupDTO')
+export class SignupDTO {
+  @Field(() => String)
+  email: string;
+
+  @Field(() => String)
+  password: string;
+
+  @Field(() => String)
+  name: string;
+
+  static zodSchema = z.object({
+    email: z.string().email().nonempty(),
+    password: z.string().min(6).nonempty(),
+    name: z.string().min(1).nonempty(),
+  });
+}
 @InputType('LoginDTO')
 export class LoginDTO {
   @Field(() => String)
