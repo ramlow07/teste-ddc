a77e5aacbada67b461328a4ea7956bb53de5aa2e
ramlow07 <luamramlow@gmail.com>
2025-07-14 21:48:55 -0300

    fixing jwt logic

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card } from "@/components/ui/card";
 import { Eye, EyeOff, Shield, ArrowRight } from "lucide-react";
-import { LOGIN_MUTATION } from "@/lib/graphqlClient";
+import { LOGIN_MUTATION } from "@/lib/graphql/graphqlClient";
 import { GraphQLClient } from "graphql-request";
 import { useRouter } from "next/navigation";
 
@@ -63,7 +63,7 @@ export default function LoginPage() {
       } else {
         console.log("Login bem-sucedido:", loginResponse.token);
         localStorage.setItem("token", loginResponse.token);
-        router.push("/dashboard"); // redireciona
+        router.push("/"); // redireciona pra main page
       }
     } catch (error) {
       console.error("Erro na requisição:", error);
