42682e8c920faea16cb7af548f3398b84d1152c4
ramlow07 <luamramlow@gmail.com>
2025-07-14 21:50:11 -0300

    fixing secret key typing mistake

--- backend/src/modules/auth/module.ts
+++ backend/src/modules/auth/module.ts
@@ -3,16 +3,17 @@ import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../infra/database/database.module';
 import { AuthService } from './service';
 import { AuthResolver } from './controller';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     DatabaseModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'your-secret-key',
+      secret: process.env.JWT_SECRET_KEY || 'ddc-secret-key',
       signOptions: { expiresIn: '1h' },
     }),
   ],
-  providers: [AuthService, AuthResolver],
-  exports: [AuthService],
+  providers: [AuthService, AuthResolver, JwtStrategy],
+  exports: [AuthService, AuthResolver, JwtStrategy],
 })
 export class AuthModule {}
